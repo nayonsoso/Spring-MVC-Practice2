ae964ce53b217e5a2ed7bb2fcba33bb2bd1bbcf6
nayonsoso <panda0329@naver.com>
2023-08-11 20:00:00 +0900

    @Value 복습, README.md 수정

---
+++ src/main/java/com/example/account/lombok/LombokTest.java
@@ -0,0 +1,14 @@
+package com.example.account.lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+public class LombokTest {
+    public int age;
+    public String name;
+
+    LombokTest(int age){
+
+    }
+}
--- src/main/java/com/example/account/service/RedisTestService.java
+++ src/main/java/com/example/account/service/RedisTestService.java
@@ -32,6 +32,4 @@ public class RedisTestService {
         }
         return "Lock success";
     }
-
-
 }
